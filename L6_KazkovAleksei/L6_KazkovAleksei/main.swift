fd20ef031a446837ad4638ea02ff8baa92d9b3f0
CristopherJohnson <henkok127@mail.ru>
2018-12-10 13:22:25 +0300

    Сделал первые 2 задания

---
+++ L6_KazkovAleksei/L6_KazkovAleksei/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L6_KazkovAleksei
+//
+//  Created by Алексей Казаков on 10/12/2018.
+//  Copyright © 2018 Алексей Казаков. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
